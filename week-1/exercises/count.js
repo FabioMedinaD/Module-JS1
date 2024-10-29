4094b2b32a71de6687d4d43fa9084e979e5b30c0
Ricardo Fabio Medina <164503593+FabioMedinaD@users.noreply.github.com>
2024-10-29 19:51:07 +0100

    Update count.js

--- week-1/exercises/count.js
+++ week-1/exercises/count.js
@@ -1,6 +1,8 @@
 let count = 0;
 
 count = count + 1;
+//Line 3 causes the variable "count" to increase by one.
+
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
