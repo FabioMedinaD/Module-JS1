b8a658056ef8fe5f2efc4485bd2a0615aa5d9a14
Ricardo Fabio Medina <164503593+FabioMedinaD@users.noreply.github.com>
2024-10-29 22:52:47 +0100

    Update 2.js

--- week-1/errors/2.js
+++ week-1/errors/2.js
@@ -1,5 +1,14 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
-
+/*
+ERROR:
 console.log(`I was born in ${cityOfBirth}`);
 const cityOfBirth = "Bolton";
+*/
+
+// It is necessary to  declare a variable "cityOfBirth " before calling it.
+
+
+const cityOfBirth = "Bolton";
+
+console.log(`I was born in ${cityOfBirth}`);
