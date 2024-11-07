419868b1c3b84fc9010c16ba8467e83b4f38fa1d
Ricardo Fabio Medina <164503593+FabioMedinaD@users.noreply.github.com>
2024-11-07 13:34:53 +0100

    Update 2.js

--- week-2/debug/2.js
+++ week-2/debug/2.js
@@ -12,3 +12,13 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+
+//The problem is that the variable is declared as a constant, That`s is why the result of the function "getLastDigit" is always the same number(3) 
+
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
