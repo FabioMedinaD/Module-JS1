5d531e288818cb5bd67de91506240be0d9951f90
Ricardo Fabio Medina <164503593+FabioMedinaD@users.noreply.github.com>
2024-11-07 13:20:22 +0100

    Update 0.js

--- week-2/debug/0.js
+++ week-2/debug/0.js
@@ -5,3 +5,10 @@ function multiply(a, b) {
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+//The difference between console.log and return it`s that "console.log" only shows the operations but does not save the result instead "return" yes, is does.
+function multiply(a, b) {
+  return (a * b);
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
