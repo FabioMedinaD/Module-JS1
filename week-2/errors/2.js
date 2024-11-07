b16e5709b84648c93823eee4960468dcd9406c26
Ricardo Fabio Medina <164503593+FabioMedinaD@users.noreply.github.com>
2024-11-07 12:00:33 +0100

    Update 2.js

--- week-2/errors/2.js
+++ week-2/errors/2.js
@@ -6,5 +6,11 @@
 function square(3) {
     return num * num;
 }
-
-
+//Error:
+// in this case "3" isn`t a variable, in the function square  "num" has not  yet 
+been declared .
+function square(num) {
+    return num * num;
+}
+console.log(square(3));
+    
