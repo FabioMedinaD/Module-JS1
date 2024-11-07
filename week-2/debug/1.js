8b83ac377f76ff6f5cbb38b878cc4e996dd228b1
Ricardo Fabio Medina <164503593+FabioMedinaD@users.noreply.github.com>
2024-11-07 13:23:41 +0100

    Update 1.js

--- week-2/debug/1.js
+++ week-2/debug/1.js
@@ -6,3 +6,12 @@ function sum(a, b) {
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+// The error is in the syntax of "return" the expression "a+b" never is evaluated.
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(2,3));
+
+
