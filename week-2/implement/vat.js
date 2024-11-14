eead549e012eaab9b163e3b9c380336e2c63c235
Ricardo Fabio Medina <164503593+FabioMedinaD@users.noreply.github.com>
2024-11-14 13:37:33 +0100

    Update vat.js

--- week-2/implement/vat.js
+++ week-2/implement/vat.js
@@ -8,3 +8,9 @@
 // Given a number,
 // When I call this function with a number
 // Then it returns the new price with VAT added on
+
+function priceWithVat (price) {
+    let vat =price*1.2;
+    return vat;
+}
+console.log(priceWithVat(100));
