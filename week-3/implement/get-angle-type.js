7da7017061214a3830fdd281e4221b23bc394a13
Ricardo Fabio Medina <164503593+FabioMedinaD@users.noreply.github.com>
2024-11-18 16:19:48 +0100

    Update get-angle-type.js

--- week-3/implement/get-angle-type.js
+++ week-3/implement/get-angle-type.js
@@ -21,3 +21,23 @@
 // Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
 // Then the function should return "Reflex angle"
+
+function getAngleType (angle){
+    if(angle === 90){
+        return "Right angle";        
+    }  else if (angle === 180){
+        return "Straight angle";
+    } else if (angle> 90&&angle<180){
+        return "Obtuse angle";
+    } else if (angle < 90){
+        return "Acute angle";
+    } else  if(angle>180&&angle<360){
+        return "Reflex angle";
+    }
+}
+
+console.log(getAngleType(90));
+console.log(getAngleType(92));
+console.log(getAngleType(80));
+console.log(getAngleType(180));
+console.log(getAngleType(185));
